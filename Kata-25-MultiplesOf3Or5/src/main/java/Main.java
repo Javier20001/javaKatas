a357554e8c58f7987c6edeb4b19c8e014de75107
javier <kuznikjavier@gmail.com>
2023-01-27 18:55:01 -0300

    Kata 25 se suman los numeros multiplos de 3 y 5 de un rango

---
+++ Kata-25-MultiplesOf3Or5/src/main/java/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        MultiplesOf3Or5.solution(10);
+    }
+}
---
+++ Kata-25-MultiplesOf3Or5/src/main/java/MultiplesOf3Or5.java
@@ -0,0 +1,11 @@
+class MultiplesOf3Or5{
+    public static int solution(int number) {
+        int result = 0;
+        for(int i = 0; i<number;i++){
+            if(i%3==0 || i%5==0){
+                result+=i;
+            }
+        }
+        return result;
+    }
+}
